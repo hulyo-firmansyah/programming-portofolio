091813b05663aa263c8a16b2b81e103e9e90f311
Hulyo <axusxzbitcomp@gmail.com>
2023-12-23 22:03:22 +0700

    fix type error

--- src/app/components/sections/Experience.tsx
+++ src/app/components/sections/Experience.tsx
@@ -6,16 +6,14 @@ import LANG_SELECTOR from "@/app/lang/lang";
 
 const ExperienceSection = () => {
   const { lang } = useContext(LangContext) as LangContextType;
-  const [wordlist] = useState<(typeof LANG_SELECTOR)["eng"]>(
-    LANG_SELECTOR[`${lang.lang}`]
-  );
+  const [wordlist] = useState<any>(LANG_SELECTOR[`${lang.lang}`]);
 
   return (
     <section className="text-gray-400 pb-28" id="work-experience" data-section>
       <div className="font-bold uppercase text-sm tracking-widest mb-7 text-gray-100 lg:hidden">
         {wordlist.navigator[1].text}
       </div>
-      {wordlist.experiences.map((v, i) => (
+      {wordlist.experiences.map((v: any, i: any) => (
         <ExperienceCard key={i} {...v} />
       ))}
     </section>
--- src/app/components/sections/ProjectSection.tsx
+++ src/app/components/sections/ProjectSection.tsx
@@ -6,9 +6,7 @@ import LANG_SELECTOR from "@/app/lang/lang";
 
 const ProjectSection = () => {
   const { lang } = useContext(LangContext) as LangContextType;
-  const [wordlist] = useState<(typeof LANG_SELECTOR)["eng"]>(
-    LANG_SELECTOR[`${lang.lang}`]
-  );
+  const [wordlist] = useState<any>(LANG_SELECTOR[`${lang.lang}`]);
 
   return (
     <section data-section id="project" className="pb-28">
@@ -16,7 +14,7 @@ const ProjectSection = () => {
         {wordlist.navigator[2].text}
       </div>
       <div className="">
-        {wordlist.projects.map((v, i) => (
+        {wordlist.projects.map((v: any, i: any) => (
           <ProjectCard key={i} {...v} />
         ))}
       </div>
